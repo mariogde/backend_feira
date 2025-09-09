0944e028bb9237607efc893b354fd65fead1d0d0
LizangeloVasconcelos <59005420+LizangeloVasconcelos@users.noreply.github.com>
2025-09-08 21:27:32 -0300

    pequena mudança em code e comnt
    pequena mudança em code e comnt

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // src/server.js
-const app = require('./app'); // importa o app.js
+const app = require('./app'); // importa o app.js<==
 const PORT = 3000; // ou process.env.PORT, se usar variáveis de ambiente
 
 app.listen(PORT, () => {
